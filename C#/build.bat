e23496b0ce5ab15d3f29ea696eaa8ac94b6f3c56
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2021-01-24 17:34:32 +0100

    Moved skin directory

---
+++ C#/build.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+msbuild -m /t:build /p:Configuration=Release;Platform=x64 ".\SDK-CPP.sln"
